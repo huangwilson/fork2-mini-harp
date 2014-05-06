47722a857594aa69347a2d1a5a8a4351ef6e613e
hyz741 <zheyu@server.local>
2014-05-06 21:21:22 +1000

    Lesson 1-1

---
+++ bin/mini-harp.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+var createMiniHarp = require("../index")
+var app = createMiniHarp();
+var argv = require("minimist")(process.argv.slice(2));
+var port = argv.port || 4000;
+console.log("Starting mini-harp on http://localhost:" + port);
+app.listen(port);
