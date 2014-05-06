88f9e66e4ec71b389cd53ee9bbff0a5540ba162b
hyz741 <zheyu@server.local>
2014-05-06 21:33:43 +1000

    Lesson 1-2

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ var connect = require('connect')
 
 module.exports = function(){
   var app = connect();
+  app.use(function(req, res, next){
+    var url = req.url;
+    if(url=="/current-time"){
+      res.end(new Date().toISOString());
+    }else{
+      next();
+    }
+  })
   return app;
 }
